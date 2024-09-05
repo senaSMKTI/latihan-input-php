97853f50661d1d2c155cfb5db4332a0b17f33e0b
Sena Nur Acksyavi <sena@smkti.net>
2024-09-05 08:48:38 +0800

    adding 3 4 5

---
+++ soal-3/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal 3</title>
+</head>
+<body>
+    <h1>Soal 3</h1>
+
+    <?php
+        
+    ?>
+</body>
+</html>
---
+++ soal-4/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal 3</title>
+</head>
+<body>
+    <h1>Soal 4</h1>
+
+    <?php
+        
+    ?>
+</body>
+</html>
