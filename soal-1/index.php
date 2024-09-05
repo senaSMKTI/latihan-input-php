e2c0e4ee9f119cbdb9cea5aca3bd3a81dca43bc7
Sena Nur Acksyavi <sena@smkti.net>
2024-09-05 08:06:47 +0800

    first batch

---
+++ soal-1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal 1</title>
+</head>
+<body>
+    <h1>Soal 1</h1>
+    <form method="post">
+        <input type="number" name="jarijari"> <br>
+        <input type="number" name="tinggi"> <br>
+        <button type="submit">Hitung Volume</button>
+        <button type="reset">Reset</button>
+    </form>
+    <?php
+        // tambahkan perhitungan volume tabung di sini
+    ?>
+</body>
+</html>
---
+++ soal-2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal 1</title>
+</head>
+<body>
+    <h1>Soal 1</h1>
+    <form method="post">
+        <select name="pilihan">
+            <option value="">KM ke M</option>
+            <option value="">KM ke CM</option>
+            <option value="">KM ke MM</option>
+        </select>
+        <button type="submit">Konversi</button>
+    </form>
+    <?php
+        // lakukan perhitungan konversi jarak di sini
+    ?>
+</body>
+</html>
